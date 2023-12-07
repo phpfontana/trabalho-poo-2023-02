e44b515a352a7a8cd5a436711ea39a15922f60d8
Emerson Vitor Pereira da Silva <63209398+Emerson-Vitor@users.noreply.github.com>
2023-12-07 16:40:00 -0300

    Update create_db.sql

--- Database/create_db.sql
+++ Database/create_db.sql
@@ -1,52 +1,23 @@
+-- Deleta o banco de dados aula, se ele existir
+SELECT 'DROP DATABASE poo_2023_02' WHERE EXISTS (SELECT FROM pg_database WHERE datname = 'poo_2023_02')\gexec
 
-DO $$ 
-BEGIN
-    IF EXISTS (SELECT FROM pg_database WHERE datname = 'poo_2023_02') THEN
-        EXECUTE 'DROP DATABASE poo_2023_02';
-    END IF;
-END $$;
+SELECT 'CREATE DATABASE poo_2023_02' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'poo_2023_02')\gexec
 
-
-DO $$ 
-BEGIN
-    IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'poo_2023_02') THEN
-        EXECUTE 'CREATE DATABASE poo_2023_02';
-    END IF;
-END $$;
-
+-- Conectar ao banco de dados criado
 \c poo_2023_02;
 
-
+-- Instalar a extensão uuid-ossp se ainda não estiver instalada
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-
-DO $$ 
-BEGIN
-    IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'advogado') THEN
-        EXECUTE 'DROP TABLE advogado';
-    END IF;
-    IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'pessoa_juridica') THEN
-        EXECUTE 'DROP TABLE pessoa_juridica';
-    END IF;
-    IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'pessoa_fisica') THEN
-        EXECUTE 'DROP TABLE pessoa_fisica';
-    END IF;
-    IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'pessoa') THEN
-        EXECUTE 'DROP TABLE pessoa';
-    END IF;
-END $$;
-
-
-
-
+-- Tabela Pessoa
 CREATE TABLE pessoa (
-    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     nome VARCHAR(255) NOT NULL,
     cpf_cnpj VARCHAR(14) UNIQUE NOT NULL CHECK (cpf_cnpj ~* '^\d{11}|\d{14}$'), -- Regex para validar CPF ou CNPJ
     tipo CHAR(1) NOT NULL CHECK (tipo IN ('F', 'J')) -- 'F' para física, 'J' para jurídica
 );
 
-
+-- Tabela PessoaFisica
 CREATE TABLE pessoa_fisica (
     id UUID PRIMARY KEY REFERENCES pessoa(id),
     data_nascimento DATE,
@@ -54,7 +25,7 @@ CREATE TABLE pessoa_fisica (
     sexo CHAR(1) CHECK (sexo IN ('M', 'F')) -- 'M' masculino, 'F' feminino
 );
 
-
+-- Tabela PessoaJuridica
 CREATE TABLE pessoa_juridica (
     id UUID PRIMARY KEY REFERENCES pessoa(id),
     razao_social VARCHAR(255) NOT NULL,
@@ -62,11 +33,13 @@ CREATE TABLE pessoa_juridica (
     inscricao_estadual VARCHAR(20)
 );
 
+-- Tabela Advogado
 CREATE TABLE advogado (
     id UUID PRIMARY KEY REFERENCES pessoa(id),
     numero_oab VARCHAR(20) UNIQUE NOT NULL,
     estado_oab CHAR(2) NOT NULL -- UF do registro da OAB
 );
 
+-- Índices para otimização de buscas
 CREATE INDEX idx_pessoa_cpf_cnpj ON pessoa(cpf_cnpj);
 CREATE INDEX idx_advogado_numero_oab ON advogado(numero_oab);
